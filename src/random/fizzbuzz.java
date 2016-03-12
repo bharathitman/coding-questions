e66cc5578afcb9bf61e95e7e5cb8e0ea2271e3e7
Bharath Bhandarkar <bharathitman@gmail.com>
2016-03-12 18:31:13 +0530

    Added the Linked Hash Set way of getting the first non repeating character

--- src/random/fizzbuzz.java
+++ src/random/fizzbuzz.java
@@ -1,16 +1,33 @@
 package random;
 
 /**
- * Created by bharath on 1/22/2016.
+ * Created by bharath on 1/22/2016..
+ * Fizzbuzz Problem
  */
+
 public class fizzbuzz {
 
     public static void main(String[] args) {
 
-        for(int i = 1; i <= 100; i++) {
+        fizzbuzz_1(18);
 
-            if(i%3 == 0 && i%5 == 0) {
-                System.out.println("fizzbuzz");
+    }
+
+    public static void fizzbuzz_1(int n) {
+
+        for(int i = 1; i <= n; i++) {
+
+            if(i % 15 == 0) {
+                System.out.print("FIZZBUZZZ ");
+            }
+            else if(i % 5 == 0) {
+                System.out.print("BUZZ ");
+            }
+            else if(i % 3 == 0) {
+                System.out.print("FIZZ ");
+            }
+            else {
+                System.out.print(i+ " ");
             }
 
         }
